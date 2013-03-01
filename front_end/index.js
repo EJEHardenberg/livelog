6c26c4f1bb9b20223c2066661502c9e3b4f00a42
JoshuaDickerson <joshuajdickerson@gmail.com>
2013-03-01 12:25:33 -0500

    cleaning up front_end javascript

--- front_end/index.js
+++ front_end/index.js
@@ -2,54 +2,54 @@
       $(document).ready(function(){
         $('#goButton').click(function(){
           $('#sessionHolder').html(sessionID);
+          
           $('#welcome').fadeOut(function(){
             $('#session').fadeIn();
           });
+          
+          var counter = 0
+          var t = setInterval(function(){
+          
+          // grab server data
+          $.ajax({
+              type: "POST",
+              url: "http://"+basedir+"/LiveLog/front_end/LiveLogDisplay.php",
+              data: {
+                "getCurrentLog": true,
+                "sessionID": sessionID
+              },
+              success: function(data){
+                // append raw data received from server
+                $('.raw').append(data);
+                try{
+                  var obj = $.parseJSON(data);
+                }catch(err){
+                  $('.err').html(err);
+                }
+                    
+                $('.phpBlock').html("");
+                $('.javaBlock').html("");
+                $('.pythonBlock').html("");
 
-          //     $(document).ready(function(){
-              var counter = 0
-              var t = setInterval(function(){
-        //       $('#blinker').toggle();
-                $.ajax({
-                  type: "POST",
-                  url: "http://"+basedir+"/LiveLog/front_end/LiveLogDisplay.php",
-                  // url: "http://localhost/chrome_extensions/sharetosave_v2/sharetosave_server/addBookmark.php",
-                  data: {
-                    "getCurrentLog": true,
-                    "sessionID": sessionID
-                  },
-                  success: function(data){
-                    $('.raw').html(data);
-                    try{
-                      var obj = $.parseJSON(data);
-                    }catch(err){
-                      $('.err').html(err);
-                    }
-
-                    $('.phpBlock').html("");
-                    $('.javaBlock').html("");
-                    $('.pythonBlock').html("");
-
-                    for(var ii=0; ii<numPHP; ii++){
-                      $('.phpBlock').append(
+                for(var ii=0; ii<numPHP; ii++){
+                  $('.phpBlock').append(
                           "<div class='logContent phpLog'></div>"
-                        );
-                    }
+                  );
+                }
 
-                    for(var ii=0; ii<numJava; ii++){
-                      $('.javaBlock').append(
+                for(var ii=0; ii<numJava; ii++){
+                  $('.javaBlock').append(
                           "<div class='logContent javaLog'></div>"
-                        );
-                    }
+                  );
+                }
 
-                    for(var ii=0; ii<numPython; ii++){
-                      $('.pythonBlock').append(
+                for(var ii=0; ii<numPython; ii++){
+                  $('.pythonBlock').append(
                           "<div class='logContent pythonLog'></div>"
-                        );
-                    }
-                    // $('.logOutput').append("</ul>");
-                  }   
-                });
-          },300);
-        });
+                  );
+                }
+              }   // end success()
+            });  // end ajax
+          },3000); // end setinterval
+        }); // end $('go').click()
       })
