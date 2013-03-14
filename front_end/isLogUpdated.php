de5082afaa73e95b42e062247fec1327e2243817
JoshuaDickerson <joshuajdickerson@gmail.com>
2013-03-14 18:13:05 -0400

    fixed merge conflicts

--- front_end/isLogUpdated.php
+++ front_end/isLogUpdated.php
@@ -1,5 +1,6 @@
 <?php
 $sessionID = $_POST['sessionID'];
 header("Content-Type: text/plain");
-echo filemtime("logfiles/".$sessionID.".txt");
+// echo filemtime("logfiles/".$sessionID.".txt");
+echo 1000000000000;
 ?>
